89bb979145f3b37710823508c26565d2e23ee3ed
lalysacramento <lalysacramento6@gmail.com>
2025-06-17 01:15:15 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -16,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/santa-ilha-imoveis-web/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/imoveis" element={<Imoveis />} />
