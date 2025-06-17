38b67e0faf57761a958aa206b66e82cfda488175
lalysacramento <lalysacramento6@gmail.com>
2025-06-17 00:57:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,18 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Caminho base ajustado para o novo repositório
+  // Caminho base para o GitHub Pages
   base: '/santa-ilha-imoveis-web/', 
-  
+
   plugins: [react()],
-  
+
+  // Configuração para o Vite entender os caminhos com "@/"
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     }
   }
 })
-```
