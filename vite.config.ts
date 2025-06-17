16f217dda2b6e5f3d80dca2bcb8dc47e4a8004ae
lalysacramento <lalysacramento6@gmail.com>
2025-06-17 00:21:31 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
-
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  // Caminho base ajustado para o novo repositório
+  base: '/santa-ilha-imoveis-web/', 
+  
+  plugins: [react()],
+  
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+    }
+  }
+})
+```
